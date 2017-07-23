c71bcf1caa31145a57609bb90e505a0a0b5d40ac
LennixM <lennart_meyer@me.com>
2017-07-23 20:55:13 +0200

    changed ip address bug

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.get('/whoami', function(req, res) {
-  var data  = {address: req.ip,
+router.get('/api/whoami', function(req, res) {
+  var data  = {address: req.connection.remoteAddress,
                language: req.acceptsLanguages()[0],
                software: "OS: " + req.useragent.os + ", Browser: " + req.useragent.browser}
   res.json(data);
